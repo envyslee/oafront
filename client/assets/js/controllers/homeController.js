367e7c9761d1724251b081ab0804520572445dda
envyslee <leebin20@sina.cn>
2016-07-21 17:59:02 +0800

    exception

--- client/assets/js/controllers/homeController.js
+++ client/assets/js/controllers/homeController.js
@@ -23,6 +23,7 @@ define([], function () {
     //错误提示
     $scope.errorMsg='网络异常';
 
+
     var error=$stateParams['error'];
     if(error!=undefined&&error!=null&&error!=""){
        switch (error){
@@ -67,6 +68,26 @@ define([], function () {
     }else if(workPlaceId==10){//苏州凤凰
       $scope.xlsUrl="sz.xls"
     }
+
+    //异常详情
+    $scope.details={
+    }
+
+    $scope.all={
+      choose:false,//是否全选
+      reason:0,//多选原因
+    }
+
+    var map=function (id,name,date,arrive,leave,am,pm) {
+      this.id=id;
+      this.name=name;
+      this.date=date;
+      this.arriveTime=arrive;
+      this.leaveTime=leave;
+      this.am=am;
+      this.pm=pm;
+    }
+
 
     $scope.currentUser={
       name:$sessionStorage['userName'],
@@ -306,12 +327,12 @@ define([], function () {
         $scope.employeeData=data;
         commonService.LoadingEnd();
       },function (e) {
-        commonService.LoadingEnd();
         if(e.message==undefined||e.message==null){
           $scope.errorMsg='网络异常，请稍后再试';
         }else{
           $scope.errorMsg=e.message;
         }
+        commonService.LoadingEnd();
         FoundationApi.publish('errorModel','open');
       });
     }
@@ -685,6 +706,116 @@ define([], function () {
     //按条件查询异常
     $scope.exceptionQuery=function () {
       getException();
+    }
+
+
+
+
+    var submitResult=function () {
+      var c=window.confirm('确定提交？');
+      if(!c){
+        return;
+      }
+      var j=JSON.stringify($scope.details);
+      var param={
+        json:j,
+        user:$sessionStorage['userId']
+      }
+      commonService.PostRequest(url+"modifyException",param).then(function (data) {
+        getEmployeeException();
+      },function (e) {
+        $scope.exErrorMsg=e.content;
+        FoundationApi.publish('exErrorModel','open');
+      })
+    }
+
+
+    //单一原因异常结果提交
+    $scope.singleReasonSubmit=function () {
+      var hasChoosed=false;
+      for (var i=0;i<$scope.details.length;i++){
+        if($scope.details[i].choose){
+          hasChoosed=true;
+          break;
+        }
+      }
+      if (hasChoosed){
+        if($scope.all.reason==0){
+          $scope.errorMsg='未选择异常原因';
+          FoundationApi.publish('errorModel','open');
+        }else{
+          for (var j=0;j<$scope.details.length;j++){
+            if($scope.details[j].choose){
+              $scope.details[j].amReason=$scope.all.reason;
+              $scope.details[j].pmReason=$scope.all.reason;
+            }
+          }
+          submitResult();
+        }
+      }else{
+        $scope.errorMsg='未选中任何一项';
+        FoundationApi.publish('errorModel','open');
+      }
+    }
+
+
+    //全选
+    $scope.allChoose=function () {
+      if ($scope.all.choose){
+        for(var i=0;i<$scope.details.length;i++){
+          $scope.details[i].choose=true;
+        }
+      }else{
+        for(var i=0;i<$scope.details.length;i++){
+          $scope.details[i].choose=false;
+        }
+      }
+    }
+
+    //单个选中
+    $scope.singleChoose=function () {
+      for(var i=0;i<$scope.details.length;i++){
+        if(!$scope.details[i].choose){
+          $scope.all.choose=false;
+          return;
+        }
+      }
+      $scope.all.choose=true;
+    }
+
+
+    var getEmployeeException=function (employee) {
+      var param={
+        employeeId:employee.employeeId
+      };
+      commonService.PostRequest(url+"getExceptionDetail",param).then(function (data) {
+        if(data.length>0){
+          var array=[];
+          for(var i=0;i<data.length;i++){
+            var a=data[i].arriveTime==null?'未打卡':new Date(data[i].arriveTime).toLocaleTimeString();
+            var l=data[i].leaveTime==null?'未打卡':new Date(data[i].leaveTime).toLocaleTimeString();
+            array[i]=new map(data[i].id,employee.employeeName,new Date(data[i].today).toLocaleDateString(),a,l,data[i].amStatus,data[i].pmStatus);
+          }
+          $scope.details=array;
+        }else{
+          $scope.details=[];
+          FoundationApi.publish('backModel','open');
+        }
+      },function (e) {
+        $scope.errorMsg=e.message;
+        FoundationApi.publish('errorModel','open');
+      });
+    }
+
+    //进入异常详情
+    $scope.exceptionDetail=function (employee) {
+      $scope.currentTag='5';
+      getEmployeeException(employee);
+    }
+
+    //提交异常处理
+    $scope.exceptionSubmit=function () {
+      submitResult();
     }
   }
   homeController.$inject = ['$scope', '$state', '$sessionStorage','$stateParams','commonService','FoundationApi'];
